14e542a30cbae9d945c1699e21ff3180f08a267c
Felipe Patricio <francacavalcante@hotmail.com>
2024-03-07 09:52:47 -0300

    Printing possible moves

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -2,9 +2,10 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Piece;
+import boardgame.Position;
 
-public class ChessPiece extends Piece {
-	
+public abstract class ChessPiece extends Piece {
+
 	private Color color;
 
 	public ChessPiece(Board board, Color color) {
@@ -13,7 +14,11 @@ public class ChessPiece extends Piece {
 	}
 
 	public Color getColor() {
-		return color;
+		return color;	
 	}
 	
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p.getColor() != color;
+	}
 }
